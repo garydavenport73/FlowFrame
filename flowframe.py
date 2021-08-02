07d4eef0d6cfe505b7b3d95b51d37e121674397c
garydavenport73 <dovedweller@gmail.com>
2021-08-02 09:32:29 -0400

    Update and rename flowFrame.py to flowframe.py
    This adds the methods to organize the widgets on the fly without ever gridding or placing in the first place and switching back and forth on the fly, see new demo.

--- flowframe.py
+++ flowframe.py
@@ -1,88 +1,109 @@
 #
 # Gary Davenport
 # dovedweller@gmail.com
-# 7/14/2021
+# 8/2/2021
 #
 # This Object, FlowFrame inherets from Frame.
 # I added methods:
 #           'addWidget'
 #           'destroyWidgets'
-# 
+#           'organizeWidgetsWithGrid'
+#           'organizeWidgetsWithPlace'
+#           'stopOrganizingWidgets'
+# I added mode='place' and mode='grid' to choose algorithm
+#
 # Make sure the instance of the frame is made to expand
 # into parent container for the FlowFrame to work correctly.
 #
 from tkinter import Frame
 
+
 class FlowFrame(Frame):
     def __init__(self, *args, **kwargs):
-        self.mode = kwargs.pop('mode','grid')
+        self.mode = kwargs.pop('mode', 'grid')
         super().__init__(*args, **kwargs)
-        if self.mode=="place":
-            self.bind("<Configure>", lambda event:self._reorganizeWidgetsWithPlace())
+        if self.mode == "place":
+            self.bind("<Configure>",
+                      lambda event: self._reorganizeWidgetsWithPlace())
         else:
-            self.mode="grid"
-            self.bind("<Configure>", lambda event:self._reorganizeWidgetsWithGrid())
-        self.widgets=[]
+            self.mode = "grid"
+            self.bind("<Configure>",
+                      lambda event: self._reorganizeWidgetsWithGrid())
+        self.widgets = []
 
-    """
     def organizeWidgetsWithGrid(self):
+        self.stopOrganizingWidgets()
+
         slaves = self.pack_slaves()
         for slave in slaves:
-            slave.flow(mode='grid')
+            slave.pack_forget()
+        for slave in slaves:
+            slave.grid()
+
         slaves = self.place_slaves()
         for slave in slaves:
-            slave.flow(mode='grid')
+            slave.place_forget()
+        for slave in slaves:
+            slave.grid()
+
+        self.update()
+        self.update_idletasks()
 
         self._reorganizeWidgetsWithGrid()
         self.bind("<Configure>", lambda event: self._reorganizeWidgetsWithGrid())
         self._reorganizeWidgetsWithGrid()
 
-
     def organizeWidgetsWithPlace(self):
-        # _unpack_unplace_ungrid(frame)
+        self.stopOrganizingWidgets()
+
         slaves = self.pack_slaves()
         for slave in slaves:
-            slave.flow(mode='place')
+            slave.pack_forget()
+        for slave in slaves:
+            slave.place(x=0, y=0)
+
         slaves = self.grid_slaves()
         for slave in slaves:
-            slave.flow(mode='place')
+            slave.grid_forget()
+        for slave in slaves:
+            slave.place(x=0, y=0)
+
+        self.update()
+        self.update_idletasks()
 
         self._reorganizeWidgetsWithPlace()
         self.bind("<Configure>", lambda event: self._reorganizeWidgetsWithPlace())
         self._reorganizeWidgetsWithPlace()
 
-
     def stopOrganizingWidgets(self):
         self.unbind("<Configure>")
 
-    """
-            
     def addWidget(self, widget, **kwargs):
-        #get the names of all widgets and place in list
-        self.widgetChildList=[]
+        # get the names of all widgets and place in list
+        self.widgetChildList = []
         for child in self.children:
             self.widgetChildList.append(child)
 
-        #add the new widget to the list
+        # add the new widget to the list
         self.widgetChildList.append(widget)
 
-        if self.mode=="place":
+        if self.mode == "place":
             self._reorganizeWidgetsWithPlace()
         else:
             self._reorganizeWidgetsWithGrid()
 
     def destroyWidgets(self):
-        #get the names of all widgets in the frame and place in list
-        self.widgetChildList=[]
+        # get the names of all widgets in the frame and place in list
+        self.widgetChildList = []
         for child in self.children:
             self.widgetChildList.append(child)
 
-        #destroy the widgets    
-        for  i in range(len(self.children)):
+        # destroy the widgets
+        for i in range(len(self.children)):
             self.children[self.widgetChildList[i]].destroy()
 
-        #reset list to empty
-        self.widgetChildList=[]
+        # reset list to empty
+        self.widgetChildList = []
 
     def _reorganizeWidgetsWithGrid(self):
         widgetsFrame = self
@@ -93,8 +114,8 @@ class FlowFrame(Frame):
             widgetKeys.append(key)
 
         for i in range(len(widgetDictionary)):
-            if i==0: #place first widget in 0,0
-                widgetDictionary[widgetKeys[i]].grid(row=0,column=0)
+            if i == 0:  # place first widget in 0,0
+                widgetDictionary[widgetKeys[i]].grid(row=0, column=0)
             else:
                 lastWidgetsRow = widgetDictionary[widgetKeys[i-1]].grid_info()["row"]
                 lastWidgetsColumn = widgetDictionary[widgetKeys[i-1]].grid_info()["column"]
@@ -103,12 +124,12 @@ class FlowFrame(Frame):
                 if width+widgetDictionary[widgetKeys[i]].winfo_width() > widgetsFrame.winfo_width():
                     row = widgetDictionary[widgetKeys[i-1]].grid_info()["row"] + 1
                     column = 0
-                    widgetDictionary[widgetKeys[i]].grid(row=row,column=column)
+                    widgetDictionary[widgetKeys[i]].grid(row=row, column=column)
                 # if adding the widget does not go past the widget, add it to the next column same row
                 else:
                     row = widgetDictionary[widgetKeys[i-1]].grid_info()["row"]
                     column = widgetDictionary[widgetKeys[i-1]].grid_info()["column"] + 1
-                    widgetDictionary[widgetKeys[i]].grid(row=row,column=column)
+                    widgetDictionary[widgetKeys[i]].grid(row=row, column=column)
             # update to make sure widths etc accurate
             widgetsFrame.update()
             widgetsFrame.update_idletasks()
